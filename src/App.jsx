fc63dd674180321c773ccb90f68ae229d845ed92
imbingz <bingz@me.com>
2021-01-26 15:42:47 -0700

    initial setup. change app.js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Auto from './Auto';
+
+function App() {
+  return (
+    <main className="App">
+     <h1>React Hooks Practice - Autocomplete</h1>
+     <div className='auto-container'>
+        <Auto />
+     </div>
+    </main>
+  );
+}
+
+export default App;
